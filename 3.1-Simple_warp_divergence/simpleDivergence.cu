381980c491405180e2c73de63a51f3801c3ad0a5
Keerthan K <kkeerthan2005@gmail.com>
2026-04-13 01:47:39 +0530

    Optimizing programs done till 3.1

--- 3.1-Simple_warp_divergence/simpleDivergence.cu
+++ 3.1-Simple_warp_divergence/simpleDivergence.cu
@@ -1,92 +1,72 @@
-#include <cuda_runtime.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
 #include <sys/time.h>
 
-inline double seconds()
-{
+inline double seconds() {
     struct timeval tp;
     gettimeofday(&tp, NULL);
     return ((double)tp.tv_sec + (double)tp.tv_usec * 1.e-6);
 }
 
-__global__ void warmingup(float *c)
-{
+__global__ void warmingup(float *c) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     float ia, ib;
     ia = ib = 0.0f;
-    if ((tid / warpSize) % 2 == 0)
-    {
+    if ((tid / warpSize) % 2 == 0) {
         ia = 100.0f;
-    }
-    else
-    {
+    } else {
         ib = 200.0f;
     }
     c[tid] = ia + ib;
 }
 
-__global__ void mathKernel1(float *c)
-{
+__global__ void mathKernel1(float *c) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     float ia, ib;
     ia = ib = 0.0f;
-    if (tid % 2 == 0)
-    {
+    if (tid % 2 == 0) {
         ia = 100.0f;
     }
-    else
-    {
+    else {
         ib = 200.0f;
     }
     c[tid] = ia + ib;
 }
 
-__global__ void mathKernel2(float *c)
-{
+__global__ void mathKernel2(float *c) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     float ia, ib;
     ia = ib = 0.0f;
-    if ((tid / warpSize) % 2 == 0)
-    {
+    if ((tid / warpSize) % 2 == 0) {
         ia = 100.0f;
-    }
-    else
-    {
+    } else {
         ib = 200.0f;
     }
     c[tid] = ia + ib;
 }
 
-__global__ void mathKernel3(float *c)
-{
+__global__ void mathKernel3(float *c) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     float ia, ib;
     ia = ib = 0.0f;
     bool ipred = (tid % 2 == 0);
-    if (ipred)
-    {
+    if (ipred) {
         ia = 100.0f;
     }
-    if (!ipred)
-    {
+    if (!ipred) {
         ib = 200.0f;
     }
     c[tid] = ia + ib;
 }
 
-__global__ void mathKernel4(float *c)
-{
+__global__ void mathKernel4(float *c) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     float ia, ib;
     ia = ib = 0.0f;
     int itid = tid >> 5;
-    if ((itid & 0x01) == 0)
-    {
+    if ((itid & 0x01) == 0) {
         ia = 100.0f;
-    }
-    else
-    {
+    } else {
         ib = 200.0f;
     }
     c[tid] = ia + ib;
@@ -94,17 +74,13 @@ __global__ void mathKernel4(float *c)
 
 int main(int argc, char **argv)
 {
-    int dev = 0;
-    cudaDeviceProp deviceProp;
-    cudaGetDeviceProperties(&deviceProp, dev);
-    printf("%s using Device %d: %s\n", argv[0], dev, deviceProp.name);
-
     int size = 64;
     int blocksize = 64;
     if (argc > 1)
         blocksize = atoi(argv[1]);
     if (argc > 2)
         size = atoi(argv[2]);
+
     printf("Data size %d\n", size);
 
     dim3 block(blocksize, 1);
@@ -118,34 +94,40 @@ int main(int argc, char **argv)
     double iStart, iElaps;
 
     cudaDeviceSynchronize();
+
     iStart = seconds();
     warmingup<<<grid, block>>>(d_C);
     cudaDeviceSynchronize();
     iElaps = seconds() - iStart;
+
     printf("warmup      <<< %4d %4d >>> elapsed %f sec \n", grid.x, block.x, iElaps);
 
     iStart = seconds();
     mathKernel1<<<grid, block>>>(d_C);
     cudaDeviceSynchronize();
     iElaps = seconds() - iStart;
+
     printf("mathKernel1 <<< %4d %4d >>> elapsed %f sec \n", grid.x, block.x, iElaps);
 
     iStart = seconds();
     mathKernel2<<<grid, block>>>(d_C);
     cudaDeviceSynchronize();
     iElaps = seconds() - iStart;
+
     printf("mathKernel2 <<< %4d %4d >>> elapsed %f sec \n", grid.x, block.x, iElaps);
 
     iStart = seconds();
     mathKernel3<<<grid, block>>>(d_C);
     cudaDeviceSynchronize();
     iElaps = seconds() - iStart;
+
     printf("mathKernel3 <<< %4d %4d >>> elapsed %f sec \n", grid.x, block.x, iElaps);
 
     iStart = seconds();
     mathKernel4<<<grid, block>>>(d_C);
     cudaDeviceSynchronize();
     iElaps = seconds() - iStart;
+
     printf("mathKernel4 <<< %4d %4d >>> elapsed %f sec \n", grid.x, block.x, iElaps);
 
     cudaFree(d_C);
